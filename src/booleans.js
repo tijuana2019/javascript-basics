83f39592496991b0c48da4a4279fb2db4749a9af
Jo <jo_serendipity@icloud.com>
2019-10-12 16:43:43 +0100

    completed booleans 1 - 10 and passed tests

--- src/booleans.js
+++ src/booleans.js
@@ -1,41 +1,92 @@
 const negate = a => {
   // your code here
+  if (a === true) {
+    return false;
+  }
+  if (a === false) {
+    return true;
+  }
 };
 
 const both = (a, b) => {
   // your code here
+  if (a == true && b == true) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 const either = (a, b) => {
   // your code here
+  if (a == true || b == true) {
+    return true;
+  } else  {
+    return false;
+  }
+  
 };
 
 const none = (a, b) => {
   // your code here
+  if (a != true && b != true) {
+    return true;
+  } else {
+    return false;
+  }
+
 };
 
 const one = (a, b) => {
   // your code here
+  if (a == true && b != true) {
+    return true;
+  } else if (a != true && b == true) {
+      return true;
+    } else {
+    return false;
+  } 
 };
 
 const truthiness = a => {
   // your code here
+  return Boolean(a);
 };
 
 const isEqual = (a, b) => {
   // your code here
+  if (a == b) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 const isGreaterThan = (a, b) => {
   // your code here
+  if (a > b) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 const isLessThanOrEqualTo = (a, b) => {
   // your code here
+  if (a <= b) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 const isOdd = a => {
   // your code here
+  if (a % 2 !== 0) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 const isEven = a => {
