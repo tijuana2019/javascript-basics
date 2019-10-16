a6bfe9554332420fc30df69c3225a8bfff5f495d
Jo <jo_serendipity@icloud.com>
2019-10-16 20:11:04 +0100

    completed some of the Arrays challenges

--- src/arrays.js
+++ src/arrays.js
@@ -1,33 +1,48 @@
 const getNthElement = (index, array) => {
   // your code here
+  if (index <= array.length - 1) {
+    return array[index];
+  } else if (index >= array.length) {
+    index -= 4;
+    return array[index];
+  }
 };
 
 const arrayToCSVString = array => {
   // your code here
+  return array.toString();
 };
 
 const csvStringToArray = string => {
   // your code here
+  return string.split(",");
 };
 
 const addToArray = (element, array) => {
   // your code here
+  array.push(element);
 };
+
+
 
 const addToArray2 = (element, array) => {
   // your code here
+  return array.concat(element);
 };
 
 const removeNthElement = (index, array) => {
   // your code here
+  return array.splice(index, 1);
+  
 };
 
 const numbersToStrings = numbers => {
   // your code here
+  return numbers.map(n => n.toString());
 };
 
 const uppercaseWordsInArray = strings => {
-  // your code here
+  return strings.map(s => s.toUpperCase());
 };
 
 const reverseWordsInArray = strings => {
